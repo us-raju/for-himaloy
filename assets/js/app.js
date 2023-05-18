2b00f103ee820c25b2a2682c815896cba9e3deeb
ws_raju <121743222+us-raju@users.noreply.github.com>
2023-05-18 21:28:58 +0600

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,71 @@
+// preloader start 
+$(window).on("load", function () {
+  $(".preloader").fadeOut();
+});
+// preloader end 
+
+$(function () {
+//  Banner section js start 
+   $(".banner_item").slick({
+     arrows: false,
+     fade: true,
+     autoplay: true,
+     autoplaySpeed: 2000,
+     dots: true,
+     dotsClass: "banner_dots",
+   });
+//  Banner section js end
+  
+//  New Arrivals Products section js start 
+   $(".nap_slider").slick({
+     slidesToShow: 4,
+     prevArrow: '<i class="nap_slider_arrow fa-solid fa-angle-left"></i>',
+     nextArrow: '<i class="nap_slider_arrow fa-solid fa-angle-right"></i>',
+   });
+//  New Arrivals Products section js end
+  
+  // The final coundown js start
+  $(".deals_timer").countdown("2030/10/10", function (event) {
+    var $this = $(this).html(
+      event.strftime(
+        "" +
+        '<div class="deals_timer_param"><span>%D</span><span>Days</span></div>' +
+        '<span class="deals_timer_clone">:</span>' +
+        '<div class="deals_timer_param"><span>%H</span><span>Hours</span></div>' +
+        '<span class="deals_timer_clone">:</span>' +
+        '<div class="deals_timer_param"><span>%M</span><span>Minute</span></div>' +
+        '<span class="deals_timer_clone">:</span>' +
+          '<div class="deals_timer_param"><span>%S</span><span>Sec</span></div>' 
+         
+      )
+    );
+  });
+  // The final coundown js end
+
+  // deals section js start 
+  $(".deals_slider").slick({
+    prevArrow: false,
+    nextArrow: false,
+    slidesToShow: 2,
+    dots: true,
+    dotsClass:'deals_dots',
+  });
+  // deals section js end
+
+  // latest_news section start 
+  // $(".latest_news_slider").slick({
+  //   slidesToShow: 4,
+  // });
+  // latest_news section end 
+
+});
+AOS.init({
+  // initClassName: "aos-init",
+  // animatedClassName: "aos-animate",
+});
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
